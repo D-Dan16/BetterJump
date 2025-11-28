39e8c5e58790b841718cbd8797c2fedcbcfb2fd0
DirtyDan16 <138337745+DirtyDan16@users.noreply.github.com>
2025-11-28 18:26:46 +0200

    Failed attmept at trying to improve method call traversal logic to fix bugs
    Also method calls now work for both kotlin and java

--- src/main/kotlin/me/dirtydan16/betterjump/actions/navigation/inside_a_file/JumpToMethodCall.kt
+++ src/main/kotlin/me/dirtydan16/betterjump/actions/navigation/inside_a_file/JumpToMethodCall.kt
@@ -3,18 +3,22 @@ package me.dirtydan16.betterjump.actions.navigation.inside_a_file
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ScrollType
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.PsiFile
+import com.intellij.psi.*
+import com.intellij.psi.util.siblings
+import fleet.util.indexOfOrNull
 import org.jetbrains.kotlin.idea.base.codeInsight.handlers.fixers.range
 import org.jetbrains.kotlin.idea.base.codeInsight.handlers.fixers.start
-import kotlin.collections.forEach
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 
 
 sealed class GoToMethodCall() : AnAction() {
+    @Suppress("UNCHECKED_CAST")
     override fun actionPerformed(e: AnActionEvent) {
         val editor = e.getData(CommonDataKeys.EDITOR) ?: return
         val carets = editor.caretModel.allCarets
@@ -24,77 +28,117 @@ sealed class GoToMethodCall() : AnAction() {
 
         //region Get Method Calls List
         val psiFile: PsiFile = e.dataContext.getData(CommonDataKeys.PSI_FILE) ?: return
-        val ktFile = psiFile as? KtFile ?: run {
-            println("problem in finding the kotlin file"); return
-        }
-        val calls: MutableList<KtCallExpression> = mutableListOf()
-        ktFile.accept(object : KtTreeVisitorVoid() {
-            override fun visitCallExpression(expression: KtCallExpression) {
-                calls.add(expression)
-                super.visitCallExpression(expression)
+
+        val codeFile: PsiClassOwner = when (psiFile) {
+            is KtFile -> psiFile
+            is PsiJavaFile -> psiFile
+            else -> {
+                println("The programming language in this file is not supported.")
+                return
             }
-        })
-        val methodCallPositions: List<TextRange> = calls.map {
-            it.range
         }
+
+        val calls: MutableList<PsiElement> = mutableListOf()
+
+        // add logic that collects each element that is recognized as a method call to the 'calls' list
+        val fileVisitor = when (codeFile) {
+            is KtFile -> {
+                object : KtTreeVisitorVoid() {
+                    override fun visitCallExpression(expression: KtCallExpression) {
+                        calls.add(expression)
+                        super.visitCallExpression(expression)
+                    }
+                }
+            }
+            is PsiJavaFile -> {
+                object : JavaRecursiveElementVisitor() {
+                    override fun visitMethodCallExpression(expression: PsiMethodCallExpression) {
+                        calls.add(expression)
+                        super.visitMethodCallExpression(expression)
+                    }
+                }
+            }
+            else -> {
+                println("The programming language in this file is not supported.")
+                return
+            }
+        }
+
+        // Actually visit the PSI tree
+        when (codeFile) {
+            is KtFile -> codeFile.accept(fileVisitor as KtTreeVisitorVoid)
+            is PsiJavaFile -> codeFile.accept(fileVisitor as JavaRecursiveElementVisitor)
+        }
+
+        calls.sortBy { it.range.start }
+
         //endregion
 
-        carets.forEach { caret ->
+        for (caret in carets) {
+            //region Find Method caret is at
+            var methodCallIndex: Int = 0
 
-            var methodCallIndexCaretIsIn: Int = methodCallPositions.lastIndex
-            var isBetweenMethods = false
-
-            for (i in methodCallPositions.indices) {
-                val position = methodCallPositions[i]
-
+            for ((index, curMethodCall) in calls.withIndex()) {
+                val range = curMethodCall.range
                 when {
                     // Caret is inside this method call
-                    position.contains(caret.offset) -> {
-                        val nextPosition = methodCallPositions.getOrNull(i + 1)
-                        // we check if the caret isn't inside multiple method calls. if it is, we will skip until we reach the last method call the caret is at, which *should* be the innermost method call.
-                        if (nextPosition == null || !nextPosition.contains(caret.offset)) {
-                            methodCallIndexCaretIsIn = i
-                            break
-                        }
+                    range.contains(caret.offset) -> {
+                        methodCallIndex = index
+                        break
                     }
                     // Caret is before this method call
-                    position.start > caret.offset -> {
-                        methodCallIndexCaretIsIn = i - 1
-                        isBetweenMethods = true
+                    range.start > caret.offset -> {
+                        // Edge case for if we are before the first method call
+                        if (index == 0) {
+                            if (this is JumpToNextMethodCall)
+                                findOffsetAndJumpToIt(range,text,caret,editor)
+                            break
+                        }
+
+                        methodCallIndex = index-1
                         break
                     }
                 }
             }
 
-            fun topCap(index: Int): Int = index.coerceAtMost(methodCallPositions.lastIndex)
-            fun bottomCap(index: Int): Int = index.coerceAtLeast(0)
+            //endregion
 
-            val targetMethodCall: TextRange = when (this) {
-                is JumpToNextMethodCall -> methodCallPositions[topCap(methodCallIndexCaretIsIn+1)]
-                is JumpToPrevMethodCall -> {
-                    if (!isBetweenMethods)
-                        methodCallPositions[bottomCap(methodCallIndexCaretIsIn - 1)]
-                    else
-                        methodCallPositions[bottomCap(methodCallIndexCaretIsIn)]
-                }
+            //region Find target method
+            val targetMethodCall: TextRange? = when (this) {
+                is JumpToNextMethodCall -> calls.elementAtOrNull(methodCallIndex+1)?.range
+                is JumpToPrevMethodCall -> calls.elementAtOrNull(methodCallIndex-1)?.range
             }
 
-            // move to the first non-whitespace character in that found block if there is such
-            var startOfParametersIndex: Int = (targetMethodCall.start..targetMethodCall.endOffset).firstOrNull {
-                text[it] in listOf('(','{')
-            } ?: (targetMethodCall.start)
+            if (targetMethodCall == null) return
+            //endregion
 
-            startOfParametersIndex++
-
-            val startOfContentIndex = (startOfParametersIndex..targetMethodCall.endOffset).firstOrNull {
-                !(text[it].isWhitespace())
-            } ?: (startOfParametersIndex)
-
-
-            caret.moveToOffset(startOfContentIndex)
-            editor.scrollingModel.scrollToCaret(ScrollType.MAKE_VISIBLE)
+            //region Move to target method
+            findOffsetAndJumpToIt(targetMethodCall, text, caret, editor)
+            //endregion
         }
 
+    }
+
+    private fun findOffsetAndJumpToIt(
+        targetMethodCall: TextRange,
+        text: CharSequence,
+        caret: Caret,
+        editor: Editor,
+    ) {
+        // move to the first non-whitespace character in that found block if there is such
+        var startOfParametersIndex: Int = (targetMethodCall.start..targetMethodCall.endOffset).firstOrNull {
+            text[it] in listOf('(', '{')
+        } ?: (targetMethodCall.start)
+
+        startOfParametersIndex++
+
+        val startOfContentIndex = (startOfParametersIndex..targetMethodCall.endOffset).firstOrNull {
+            !(text[it].isWhitespace())
+        } ?: (startOfParametersIndex)
+
+
+        caret.moveToOffset(startOfContentIndex)
+        editor.scrollingModel.scrollToCaret(ScrollType.MAKE_VISIBLE)
     }
 
 
--- src/main/kotlin/me/dirtydan16/betterjump/utils/extensions/BetterJumpUtils.kt
+++ src/main/kotlin/me/dirtydan16/betterjump/utils/extensions/BetterJumpUtils.kt
@@ -3,6 +3,13 @@ package me.dirtydan16.betterjump.utils.extensions
 import com.intellij.ide.DataManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionManager
+import kotlin.reflect.KClass
+
+infix fun <T> T.castTo(classes: List<KClass<*>>): Any? {
+    return classes.firstOrNull { kclass ->
+        kclass.isInstance(this)
+    }?.let { if (it.isInstance(this)) this else null }
+}
 
 fun repeatAction(times: Int , action: String, editor: Editor) {
     repeat(times) {
